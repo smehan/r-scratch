858d8ffbeb78eacde6e70a59783c088440df80fa
Shawn Mehan <shawn.mehan@ieee.org>
2015-05-15 13:32:00 -0700

    Draw nubers from probability distributions including the normal, binomial, and poisson. Calculate sd, mean, correlations. Compare samples with t-tests and analysis of variance/anova.

--- C9.r
+++ C9.r
@@ -113,3 +113,75 @@ ggplot(myEconCorMeltedOrdered) +
     labs(x=NULL, y=NULL) +
     ggtitle("Correlation Heat Map of Economic Factors")
 
+# t-test
+data("tips", package="reshape2")
+# 1-sample t-test first. testing for equal to, not greater/less than
+t.test(tips$tip, alternative="two.sided", mu=2.5)
+randT <- rt(30000, df=NROW(tips)-1)
+tipTTest <- t.test(tips$tip, alternative="two.sided", mu=2.5)
+
+ggplot(data.frame(x=randT)) +
+    geom_density(aes(x=x), fill="grey", color="grey") +
+    geom_vline(xintercept=tipTTest$statistic) +
+    geom_vline(xintercept=mean(randT) + c(-2,2)*sd(randT), linetype=2)
+
+#result shows mu is far from center of distribution
+
+# 1 sided to show if less or more than
+t.test(tips$tip, alternative="greater", mu=2.5)
+
+# compare tips of females to males
+aggregate(tip~sex, data=tips, mean)
+
+#to t test effectively between two groups, need to have the variance equal, so lets test
+aggregate(tip~sex, data=tips, var)
+#appears different, but lets test for normally distributed 
+shapiro.test(tips$tip)
+# shows with low p value NOT normally distributed as a whole
+shapiro.test(tips$tip[tips$sex == "Female"])
+shapiro.test(tips$tip[tips$sex == "Male"])
+# low p values show subsets are both not normally distributed, so normal test wont work.
+# still let's plot and visualize
+ggplot(tips, aes(x=tip, fill=sex)) + 
+    geom_histogram(binwidth=0.5, alpha=1/2)
+# both are skewed. Thus, parametric test will not work. Need to use non-parametric test
+ansari.test(tip~sex, tips)
+#high p values mean we can use this test
+t.test(tip~sex, data=tips, var.equal=TRUE)
+# p value above threshold, so tips between the sexes are roughly equivalent
+# let's visualize
+library(plyr)
+tipSummary <- ddply(tips, "sex", summarize, 
+                    tip.mean=mean(tip), tip.sd=sd(tip),
+                    Lower=tip.mean - 2*tip.sd/sqrt(NROW(tip)),
+                    Upper=tip.mean + 2*tip.sd/sqrt(NROW(tip)))
+ggplot(tipSummary, aes(x=tip.mean, y=sex)) + 
+    geom_point() +
+    geom_errorbarh(aes(xmin=Lower, xmax=Upper), height=.2)
+#paired t test
+library(UsingR)
+#have data set of father and son variables
+t.test(father.son$fheight, father.son$sheight, paired=TRUE)
+# results show low pvalues, so little difference between paired observations
+#what about more than two samples? Need Anova
+
+#want to test whether tips vary by day
+tipAnova <- aov(tip ~ day -1, tips)
+tipIntercept <- aov(tip ~ day, tips)
+tipAnova$coefficients
+tipIntercept$coefficients
+# we have an intercept != 0, so can only compute on N-1 values
+# look at summary to see what is p value for anova
+summary(tipAnova)
+# let's visualize
+tipsByDay <- ddply(tips, "day", summarise,
+                   tip.mean=mean(tip), tip.sd=sd(tip),
+                   Length=NROW(tip),
+                   tfrac=qt(p=.9, df=Length-1),
+                   Lower=tip.mean - tfrac*tip.sd/sqrt(Length),
+                   Upper=tip.mean + tfrac*tip.sd/sqrt(Length))
+
+ggplot(tipsByDay, aes(x=tip.mean, y=day)) +
+    geom_point() +
+    geom_errorbarh(aes(xmin=Lower, xmax=Upper), height=.3)
+# see the non-overlapping confidence intervals

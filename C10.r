1414173acb5a4da7dd8de3a31c0fc606aade7136
Shawn Mehan <shawn.mehan@ieee.org>
2015-05-28 16:07:06 -0700

    Model quality assessment with residuals and deviance for generalized linear models. Also using Anova, AIC and BIC to assess complexity. K-fold cross validation.

--- C10.r
+++ C10.r
@@ -196,3 +196,114 @@ plot(survfit(ag1), conf.int = TRUE)
 plot(survfit(ag2), conf.int = TRUE, col=1:2)
 legend("topright", legend=c(1,2), lty=1, col=1:2, text.col=1:2, title="rx")
 
+# assess model quality with residuals
+
+# fit a lm on housing data
+house1 <- lm(ValuePerSqFt ~ Units + SqFt + Boro, data=housing)
+# residual diagnostic info available with fortify function
+head(fortify(house1))
+
+ggplot(aes(x=.fitted, y=.resid), data=house1) +
+    geom_point() +
+    geom_hline(yintercetp = 0) +
+    geom_smooth(se=FALSE) +
+    labs(x="Fitted Values", y="Residuals") +
+    geom_point(aes(color=Boro))
+# Q-Q plot
+# using base graphics
+plot(house1, which=2)
+# note that we get deviance from the straight fit, especially at the tails. Indicates a poor fit
+# recreate same plot in ggplot
+ggplot(house1) +
+    aes(sample=.stdresid)) +
+    stat_qq() +
+    geom_abline()
+
+#want residuals to be normally distributed, so lets make a histogram of them
+ggplot(house1) +
+    aes(x=.resid) +
+    geom_histogram()
+
+#not quite as random as we would like.
+
+# lets construct several models to compare
+
+house1 <- lm(ValuePerSqFt ~ Units + SqFt + Boro, housing)
+house2 <- lm(ValuePerSqFt ~ Units * SqFt + Boro, housing)
+house3 <- lm(ValuePerSqFt ~ Units + SqFt * Boro + Class, housing)
+house4 <- lm(ValuePerSqFt ~ Units + SqFt * Boro + SqFt*Class, housing)
+house5 <- lm(ValuePerSqFt ~ Boro + Class, housing)
+
+multiplot(house1, house2, house3, house4, house5, pointSize = 2)
+
+# now lets get some numerical analysis with anova
+
+anova(house1, house2, house3, house4, house5)
+
+#Low RSS is a good indicator of quality, but also could be also a problem with overfitting with multiple variables
+#Let's use AIC/BIC for multiple coefficients to penalize for complexity
+
+AIC(house1, house2, house3, house4, house5)
+BIC(house1, house2, house3, house4, house5)
+
+# Anova, AIC and BIC all point to house4
+
+# when working with glms, we don't use residuals, but rather deviance. 
+# let's build up five glms and compare them with deviance.
+
+housing$HighValue <- housing$ValuePerSqFt >= 150 # returns T/F for those observations
+
+high1 <- glm(HighValue ~ Units + SqFt + Boro, housing, family=binomial(link="logit"))
+high2 <- glm(HighValue ~ Units * SqFt + Boro, housing, family=binomial(link="logit"))
+high3 <- glm(HighValue ~ Units + SqFt * Boro + Class, housing, family=binomial(link="logit"))
+high4 <- glm(HighValue ~ Units + SqFt * Boro + SqFt*Class, housing, family=binomial(link="logit"))
+high5 <- glm(HighValue ~ Boro + Class, housing, family=binomial(link="logit"))
+
+# now look at deviances
+
+anova(high1, high2, high3, high4, high5)
+AIC(high1, high2, high3, high4, high5)
+BIC(high1, high2, high3, high4, high5)
+
+# Now lets validate with k-fold validation
+# with a K=10, we can train on 9, test on 1, and track the errors, then rotate.
+
+library(boot)
+
+# build a model
+houseG1 <- glm(ValuePerSqFt ~ Units + SqFt + Boro, data=housing, family=gaussian(link="identity"))
+
+# now cross validate
+houseCV1 <- cv.glm(housing, houseG1, K=5)
+houseCV1$delta #validated error and adjusted validated error
+
+# errors are similar, so we can leave out leave-1-out cross validation
+# this is useful for comparing one model to another
+
+houseG2 <- glm(ValuePerSqFt ~ Units*SqFt + Boro, data=housing, family=gaussian(link="identity"))
+houseG3 <- glm(ValuePerSqFt ~ Units + SqFt*Boro + Class, data=housing, family=gaussian(link="identity"))
+houseG4 <- glm(ValuePerSqFt ~ Units + SqFt*Boro + SqFt*Class, data=housing, family=gaussian(link="identity"))
+houseG5 <- glm(ValuePerSqFt ~ Boro + Class, data=housing, family=gaussian(link="identity"))
+
+houseCV2 <- cv.glm(housing, houseG2, K=5)
+houseCV3 <- cv.glm(housing, houseG3, K=5)
+houseCV4 <- cv.glm(housing, houseG4, K=5)
+houseCV5 <- cv.glm(housing, houseG5, K=5)
+
+# Now, with the cross validation performed and saved, lets place results in a single df for analysis
+
+cvResults <- as.data.frame(rbind(houseCV1$delta, houseCV2$delta, 
+                                 houseCV3$delta, houseCV4$delta, houseCV5$delta))
+
+names(cvResults) <- c("Error", "Adjusted Error")
+cvResults$Model <- sprintf("houseG%s", 1:5)
+
+cvAnova <- anova(houseG1, houseG2, houseG3, houseG4, houseG5)
+cvAIC <- AIC(houseG1, houseG2, houseG3, houseG4, houseG5)
+cvBIC <- BIC(houseG1, houseG2, houseG3, houseG4, houseG5)
+
+cvResults$Anova <- cvAnova$'Resid. Dev'
+cvResults$AIC <- cvAIC$AIC
+cvResults$BIC <- cvBIC$BIC
+
+cvResults

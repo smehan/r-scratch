08aafaa1c990d13de742aee4cd62b9d8c45e073f
Shawn Mehan <shanw.mehan@ieee.org>
2015-08-21 14:32:40 -0700

    Moved to a sub-dir.

---
+++ Networks/Network-src-loader.py
@@ -0,0 +1,51 @@
+__author__ = 'shawnmehan'
+
+import csv, re
+import xml.etree.cElementTree
+
+
+def write_csv(data):
+    outfile = open('./Dataset1-Media-Example-EDGES.csv', 'wb')
+    outwriter = csv.writer(outfile, delimiter=',')
+    count = 0
+    for d in data:  # iterate through each row
+        outwriter.writerow(d)  # write the row
+        count += 1
+    print u"There were {0:d} lines processed in this run.".format(count)
+    outfile.close()
+
+
+def get_node_id(node, node_id):
+    if (node.attrib.get('id')):
+        node_id = node.attrib.get('id')  # set a new node_id if avail
+    return(node_id)
+
+
+def get_row(child, child_id, r, isNew):
+    if isNew:
+        r.append(child_id)
+        isNew = False  # boolean to mark whether we have a new block
+    else:
+        r.append(child.text)
+    return (r, isNew)
+
+
+def recursive_child(parent):
+    child_id = ''  # initialize the child_id
+    new_row = True  # initialize boolean control
+    row = []  # initialize row and build up data from <tr>
+    for child in parent:
+        child_id = get_node_id(child, child_id)
+        row, new_row = get_row(child, child_id, row, new_row)
+        if len(row) == 5:
+            data.append(row)  # add previous complete row to output
+            row = []  # reset row for new row.
+
+data = [['id', 'from', 'to', 'weight', 'type']]
+with open('./src.txt', mode='rt') as f:
+    tree = xml.etree.ElementTree.parse(f)
+for child in tree.iterfind('tbody/'):
+    recursive_child(child)
+write_csv(data)
+
+
